5bfec03138a6f937a7b202d67dc61501e22e4d15
Fatih <fatihdurmaz.93@gmail.com>
2022-09-11 11:18:21 +0300

    Initial Commit

---
+++ Note App/Note_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note_AppApp.swift
+//  Note App
+//
+//  Created by Kodgem Technology on 11.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Note_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
